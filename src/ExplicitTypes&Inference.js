3d66e69802f05a54c12a7700e71a20e2a8bed6c9
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-11-16 13:45:57 +0300

    Added a JSON.stringify instance

--- src/ExplicitTypes&Inference.js
+++ src/ExplicitTypes&Inference.js
@@ -1,77 +1,61 @@
 // 1.Basic Explicit Types
 // String
-let greeting: string = "Hello, TypeScript!";
-
+var greeting = "Hello, TypeScript!";
 // Number
-let userCount: number = 42;
-
+var userCount = 42;
 // Boolean
-let isLoading: boolean = true;
-
+var isLoading = true;
 // Array of numbers
-let scores: number[] = [100, 95, 98];
-
+var scores = [100, 95, 98];
 // Functions with Explicit Types
-function greet(name: string): string {
-    return `Hello ${name}`
+function greet(name) {
+    return "Hello ".concat(name);
 }
 greet("JJ");
-
 // 2. Type Inference
 // TypeScript can automatically determine (infer) the type of a variable based on its initial value:
 // TypeScript infers 'string'
-let username = "alice";
-
+var username = "alice";
 // TypeScript infers 'number'
-let score = 100;
-
+var score = 100;
 // TypeScript infers 'boolean[]'
-let flags = [true, false, true];
-
+var flags = [true, false, true];
 // TypeScript infers return type as 'number'
-function add(a: number, b: number) {
-    return a+b
+function add(a, b) {
+    return a + b;
 }
 add(3, 9);
-
 // Object Literal inference
-const user = {
+var user = {
     name: "Klow",
     age: 60,
     isAdmin: true
-}
+};
 console.log(user.age);
-
 // Explicit type Mismatch
-let username2: string = "alice";
+var username2 = "alice";
 // username2 = 42;  Error: Type 'number' is not assignable to type 'string'
-
 // JS behavior
 // function add2(a, b) {
 //     return a+b
 // }
 // console.log(add2("3", "5"));
-
 // TypeScript catches these issues at compile time:
-function add2(a:number, b:number):number {
-    return a+b
+function add2(a, b) {
+    return a + b;
 }
 console.log(add2(3, 5));
-
 // Common cases for type any:
 // 1. JSON.parse returns 'any' because the structure isn't known at compile time
-const data = JSON.parse('{"name":"Yolo", "age":"70"}')
+var data = JSON.parse('{"name":"Yolo", "age":"70"}');
 console.log(data);
-
-const data2 = {
+var data2 = {
     name: "Jimbo",
     age: 77,
-    isAdmin:false
-}
+    isAdmin: false
+};
 console.log(JSON.stringify(data2));
-
-
 // 2. Variables declared without initialization
-let something;
+var something;
 something = 49;
 something = "Yes Yes";
