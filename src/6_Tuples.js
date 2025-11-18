3ea14e4a69b102989328c27ea3cff2e66fa2030e
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-11-18 12:03:20 +0300

    Tuples

---
+++ src/6_Tuples.js
@@ -0,0 +1,11 @@
+// A tuple is a types array with pre-defined length and types for each index
+var ourTuple = ["Jlow", 57, true];
+console.log((ourTuple));
+// Named tuples:
+// Named tuples allow us to provide context for our values at each index
+var graph = [3.4, 8.9];
+console.log(graph);
+// Destructuring tuples
+var graph2 = [3.4, 8.9];
+var x = graph2[0], y = graph2[1];
+console.log(x, y);
