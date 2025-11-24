d08896da562330e70a401f3f53c20888a0ed157d
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-11-24 12:44:54 +0300

    Generics in Classes

--- src/14_Generics.js
+++ src/14_Generics.js
@@ -4,3 +4,21 @@ var createPair = function (first, second) {
     return [first, second];
 };
 console.log(createPair("Yellow", 4));
+// In Classes
+var NamedValue = /** @class */ (function () {
+    function NamedValue(name) {
+        this.name = name;
+    }
+    NamedValue.prototype.SetValue = function (value) {
+        return this._value;
+    };
+    NamedValue.prototype.getValue = function () {
+        return this._value;
+    };
+    NamedValue.prototype.toString = function () {
+        return "".concat(this.name, ":").concat(this._value);
+    };
+    return NamedValue;
+}());
+var value = new NamedValue("Yellow");
+console.log(value.toString());
--- src/14_Generics.ts
+++ src/14_Generics.ts
@@ -5,3 +5,25 @@ const createPair = <x, y>(first: x, second: y): [x, y] => {
     return [first, second];
 }
 console.log(createPair<string, number>("Yellow", 4));
+
+// In Classes
+class NamedValue<T>{
+    private _value : T | undefined;
+
+    constructor(private name: string) { }
+    
+    public SetValue(value: string) {
+        return this._value
+    }
+
+    public getValue(): T | undefined{
+        return this._value;
+    }
+
+    public toString(): string{
+        return `${this.name}:${this._value}`;
+    }
+
+}
+let value = new NamedValue<number>("Yellow");
+console.log(value.toString());
