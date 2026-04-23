9633dc3dee99ecc483c743dbcb2d03c7b4ce2ae5
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-04-23 20:12:49 +0300

    Chore

--- src/3_ExplicitTypes&Inference.ts
+++ src/3_ExplicitTypes&Inference.ts
@@ -1,49 +1,49 @@
 // 1.Basic Explicit Types
 // String
-let greeting: string = "Hello, TypeScript!";
+let greeting: string = 'Hello, TypeScript!'
 
 // Number
-let userCount: number = 42;
+let userCount: number = 42
 
 // Boolean
-let isLoading: boolean = true;
+let isLoading: boolean = true
 
 // Array of numbers
-let scores: number[] = [100, 95, 98];
+let scores: number[] = [100, 95, 98]
 
 // Functions with Explicit Types
 function greet(name: string): string {
-    return `Hello ${name}`
+  return `Hello ${name}`
 }
-greet("JJ");
+greet('JJ')
 
 // 2. Type Inference
 // TypeScript can automatically determine (infer) the type of a variable based on its initial value:
 // TypeScript infers 'string'
-let username = "alice";
+let username = 'alice'
 
 // TypeScript infers 'number'
-let score = 100;
+let score = 100
 
 // TypeScript infers 'boolean[]'
-let flags = [true, false, true];
+let flags = [true, false, true]
 
 // TypeScript infers return type as 'number'
 function add(a: number, b: number) {
-    return a+b
+  return a + b
 }
-add(3, 9);
+add(3, 9)
 
 // Object Literal inference
 const user = {
-    name: "Klow",
-    age: 60,
-    isAdmin: true
+  name: 'Klow',
+  age: 60,
+  isAdmin: true,
 }
-console.log(user.age);
+console.log(user.age)
 
 // Explicit type Mismatch
-let username2: string = "alice";
+let username2: string = 'alice'
 // username2 = 42;  Error: Type 'number' is not assignable to type 'string'
 
 // JS behavior
@@ -53,25 +53,25 @@ let username2: string = "alice";
 // console.log(add2("3", "5"));
 
 // TypeScript catches these issues at compile time:
-function add2(a:number, b:number):number {
-    return a+b
+function add2(a: number, b: number): number {
+  return a + b
 }
-console.log(add2(3, 5));
+console.log(add2(3, 5))
 
 // Common cases for type any:
 // 1. JSON.parse returns 'any' because the structure isn't known at compile time
+// JSON.parse() converts a JSON string into a normal JavaScript/TypeScript object that your code can use. Its opposite is JSON.stringify()
 const data = JSON.parse('{"name":"Yolo", "age":"70"}')
-console.log(data);
+console.log(data)
 
 const data2 = {
-    name: "Jimbo",
-    age: 77,
-    isAdmin:false
+  name: 'Jimbo',
+  age: 77,
+  isAdmin: false,
 }
-console.log(JSON.stringify(data2));
-
+console.log(JSON.stringify(data2))
 
 // 2. Variables declared without initialization
-let something;
-something = 49;
-something = "Yes Yes";
+let something
+something = 49
+something = 'Yes Yes'
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+const data = JSON.parse('{"name":"Yolo", "age":"70"}')
+console.log(data)
+const objData = {
+  name: 'Mercedes',
+  model: 'GLE',
+  year: 2020,
+}
+console.log(JSON.stringify(objData))
